e4a4780bbf804cb64170664d9b5a0ce5efb5c6d0
martin <martinshek8@gmail.com>
2018-07-18 18:58:05 +0100

    Added Polmont

--- homework_day3.rb
+++ homework_day3.rb
@@ -4,4 +4,19 @@ stops.push ("Edinburgh Waverly")
 
 stops.unshift ("Glasgow Queen St")
 
+stops.insert(4, 'Polmont')
+
 p stops
+
+
+
+# Add "Edinburgh Waverley" to the end of the array
+# Add "Glasgow Queen St" to the start of the array
+# Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+# Work out the index position of "Linlithgow"
+# Remove "Livingston" from the array using its name
+# Delete "Cumbernauld" from the array by index
+# How many stops there are in the array?
+# How many ways can we return "Falkirk High" from the array?
+# Reverse the positions of the stops in the array
+# Print out all the stops using a for loop
